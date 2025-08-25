04048a1df8977ef8de3621384445ab6228711631
Charles Beynon <eToThePiIPower@gmail.com>
2025-08-24 22:12:07 -0400

    Add password strategy to AshAuthentication

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule Librex.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:picosat_elixir, "~> 0.2"},
       {:ash_authentication, "~> 4.0"},
       {:ash_phoenix, "~> 2.0"},
